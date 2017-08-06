8040003853d389405c494ee82e550d975c8a87d9
Piotr Wittchen <piotr@wittchen.biz.pl>
2017-08-06 09:00:13 +0200

    updating battery_level.sh script

--- scripts/battery_level.sh
+++ scripts/battery_level.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
 
-main() {
-  if [ `uname` = "Linux" ]; then
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source "$CURRENT_DIR/helpers.sh"
+
+print_battery_level() {
+  if is_linux; then
     batteryLevelLinux=$(cat /sys/class/power_supply/BAT0/capacity)
     echo $batteryLevelLinux'%'
   fi
 
-  if [ `uname` = "Darwin" ]; then
+  if is_osx; then
     pmset -g batt | awk '{print $3}' | sed 's/;//' | tail -n+2
   fi
+}
+
+main() {
+  print_battery_level
 }
 
 main
