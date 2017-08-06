2625aceec12473a87793d826191af014f115e78b
Piotr Wittchen <piotr@wittchen.biz.pl>
2017-08-06 09:28:37 +0200

    updating helpers.sh - removing unused stuff

--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -12,36 +12,14 @@ get_tmux_option() {
 }
 
 is_osx() {
-	[ $(uname) == "Darwin" ]
+  [ $(uname) == "Darwin" ]
 }
 
 is_linux() {
-	[ $(uname) == "Linux" ]
-}
-
-is_chrome() {
-	chrome="/sys/class/chromeos/cros_ec"
-	if [ -d "$chrome" ]; then
-		return 0
-	else
-		return 1
-	fi
+  [ $(uname) == "Linux" ]
 }
 
 command_exists() {
-	local command="$1"
-	type "$command" >/dev/null 2>&1
-}
-
-battery_status() {
-	if command_exists "pmset"; then
-		pmset -g batt | awk -F '; *' 'NR==2 { print $2 }'
-	elif command_exists "upower"; then
-		# sort order: attached, charging, discharging
-		for battery in $(upower -e | grep battery); do
-			upower -i $battery | grep state | awk '{print $2}'
-		done | sort | head -1
-	elif command_exists "acpi"; then
-		acpi -b | grep -oi 'discharging' | awk '{print tolower($0)}'
-	fi
+  local command="$1"
+  type "$command" >/dev/null 2>&1
 }
